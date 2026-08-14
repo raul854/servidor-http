e3935fb6adc67d6dd763515260d69e09253af3f5
raulll-spoted <raul.barbosa@estudante.ifms.edu.br>
2026-08-14 20:56:18 +0000

    slide03

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -1,28 +1,16 @@
 import http from 'node:http'
-import { URL } from 'node:url'
-
-const porta = 3000
+const PORT = 3000
 
 const server = http.createServer();
 
-const requisicao = (req, res) => {
+server.on('request', (req, res) => {
+    console.log(`[${new Date().toISOString()}] Requisição recebida! ${req.method} ${req.url}`);
+
+    res.statusCode = 201;
     res.setHeader('Content-Type', 'application/json');
-    res.statusCode = 200
-    const urlObj = new URL(req.url, `http://${req.headers.host}`);
+    res.end(JSON.stringify({ status: "ok" }));
+});
 
-    if (req.method === 'GET' && urlObj.pathname === '/saudacao') {
-        const nome = urlObj.searchParams.get('nome');
-        return res.end(JSON.stringify({ "nome": nome }));
-    }
-
-    return res.end(JSON.stringify({ "chave": "valor" }));
-
-    console.log(`Requisição recebida! ${req.method} ${req.url}`);
-    res.end();
-}
-
-server.on('request', requisicao);
-
-server.listen(porta, () => {
-    console.log(`Servidor ouvindo na porta ${porta}`)
-});			
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
